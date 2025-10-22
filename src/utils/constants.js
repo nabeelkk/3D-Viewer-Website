8c68d80c8c66419fb589435fb33a8eff62588cbc
nabeelkk <muhammednabeelkk@Muhammeds-MacBook-Air-2.local>
2025-10-23 04:30:33 +0530

    Update BaseApi

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:5003/api';
+export const API_BASE = 'https://threed-viewer-website-server.onrender.com/api';
 
 export const MAX_FILE_SIZE = 100 * 1024 * 1024; 
 
